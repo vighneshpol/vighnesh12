d9dfb630308e2ffb3eaaab76b6a9d4ea3c149e90
“vighneshpol” <vighnesh.pol@pantheon.io>
2022-08-11 14:19:43 +0530

    cleared useless codes

--- README.md
+++
@@ -1,1 +0,0 @@
-# vighnesh12
--- ppname.js
+++ ppname.js
@@ -9,28 +9,55 @@ const { remote } = require('webdriverio');
 
     await browser.url('https://dashboard.pantheon.io/login')
     
-    const abcd = await browser.$('button#auth0login');
-    await abcd.click();
+    const submit1 = await browser.$('button#auth0login');
+    await submit1.click();
 
-    const gmail=await browser.$("//input[@id='1-email']");
-    await gmail.waitForDisplayed({ timeout: 10000 });
-    await gmail.setValue('testvighu@gmail.com');
+    const email=await browser.$("input[type='email']");
+    await email.waitForDisplayed({ timeout: 10000 });
+    await email.setValue('testvighu@gmail.com');
 
-    const gmail1=await browser.$("//input[@id='1-password']");
-    await gmail1.waitForDisplayed({ timeout: 10000 });
-    await gmail1.setValue('Testvighu@gmail.com');
 
-    const abcdefg = await browser.$('//*[@id="1-submit"]');
-    await abcdefg.click();
+    const password=await browser.$("input[type='password']");
+    await password.waitForDisplayed({ timeout: 10000 });
+    await password.setValue('Testvighu@gmail.com');
 
+    const smtBtn = await browser.$('//*[@id="1-submit"]');
+    await smtBtn.click();
+
+    // const search=await browser.$('#downshift-0-input');
+    // await search.waitForDisplayed({ timeout: 100000 });
+    // await expect(search).toBeDisplayed();
+    // await expect($('#flash')).toBeExisting();
+
+    const dropdown = await browser.$('.css-1acf2q9-noButtonStyles-navbarLink-AccountDropdown');
+    await dropdown.waitForDisplayed({ timeout: 10000 });
+    await dropdown.click();
+
+    const logout = await browser.$("a[href='/logout']");
+    await logout.click();
+
+
+    // browser.closeWindow()
     // elem = await browser.$('div#identifierNext');
+    // isDisplayed = await elem.isDisplayed(); .css-1acf2q9-noButtonStyles-navbarLink-AccountDropdown
+    
+    // elem = await browser.$("//span[text()='vighnesh pol']");
+    // await elem.waitForDisplayed({ timeout: 100000 });
     // isDisplayed = await elem.isDisplayed();
+    // console.log(isDisplayed);
 
-    elem = await browser.$("//span[text()='vighnesh pol']");
-    await elem.waitForDisplayed({ timeout: 100000 });
-    isDisplayed = await elem.isDisplayed();
-    console.log(isDisplayed);
 
+    /*
+    Clickable 
+
+    const el = await browser.$('//*[@id="sites-page"]/div/div[1]/a[1]')
+    let clickable = await el.isClickable();
+    console.log(clickable); // outputs: true or false
+
+    // wait for element to be clickable
+    await browser.waitUntil(() => el.isClickable())
+
+    */
 
     /*const abcde = await browser.$('input[aria-label="Email"]');
     await abcde.setValue('vigupol@gmail.com');*/
